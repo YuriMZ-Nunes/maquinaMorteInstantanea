aba564d62bc417aa554bd1e11adc352dd330c609
Vinícius <viniciushallmann2015@gmail.com>
2023-11-24 21:58:41 -0300

    commit codigo RightUpper refatorado

--- src/RightUpper.java
+++ src/RightUpper.java
@@ -1,50 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class RightUpper {
 
     public static void configure(JPanel panel) {
         panel.setBorder(BorderFactory.createTitledBorder("Registradores"));
-        panel.setLayout(new GridLayout(5, 2, 5, 5)); // Layout com 6 linhas, 2 colunas e espaçamento
+        panel.setLayout(new GridLayout(5, 2, 5, 5)); 
+        Map<String, String> registradores = new HashMap<>(Map.of("A", "12", "X", "6", "L", "0XC001", "PC", "22", "SW", "0xc001F"));
 
-        // Adicionando rótulos para os nomes dos registradores
-        JLabel labelRegistrador1 = new JLabel("            A :");
-        JLabel labelRegistrador2 = new JLabel("            X :");
-        JLabel labelRegistrador3 = new JLabel("            L :");
-        JLabel labelRegistrador4 = new JLabel("            PC :");
-        JLabel labelRegistrador5 = new JLabel("            SW :");
+        registradores.entrySet().forEach(entry -> {
+            JLabel reg_key = new JLabel(entry.getKey() + " :");
+                reg_key.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+            panel.add(reg_key);
 
-        // Adicionando caixas de texto para exibir os valores dos registradores
-        JTextField reg_A_value = new JTextField();
-        JTextField reg_X_value = new JTextField();
-        JTextField reg_L_value = new JTextField();
-        JTextField reg_PC_value = new JTextField();
-        JTextField reg_SW_value = new JTextField();
-
-        // Configurando caixas de texto como não editáveis
-        reg_A_value.setEditable(false);
-        reg_X_value.setEditable(false);
-        reg_L_value.setEditable(false);
-        reg_PC_value.setEditable(false);
-        reg_SW_value.setEditable(false);
-
-        //Adicionando Exemplos
-        reg_A_value.setText("12");
-        reg_X_value.setText("6");
-        reg_L_value.setText("0XC001");
-        reg_PC_value.setText("22");
-        reg_SW_value.setText("0xc001F");
-
-        // Adicionando rótulos e caixas de texto ao painel
-        panel.add(labelRegistrador1);
-        panel.add(reg_A_value);
-        panel.add(labelRegistrador2);
-        panel.add(reg_X_value);
-        panel.add(labelRegistrador3);
-        panel.add(reg_L_value);
-        panel.add(labelRegistrador4);
-        panel.add(reg_PC_value);
-        panel.add(labelRegistrador5);
-        panel.add(reg_SW_value);
+            JLabel reg_value = new JLabel(entry.getValue());
+                reg_value.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+            panel.add(reg_value);
+        });
     }
 }
