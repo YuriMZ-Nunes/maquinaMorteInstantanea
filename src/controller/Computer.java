8651cdb1de2be855ec8465fc43f741f9fdee8cd4
YuriMZ-Nunes <yuri.m.z.nunes@gmail.com>
2023-11-26 07:04:32 -0300

    fiz minhas instruções

--- src/controller/Computer.java
+++ src/controller/Computer.java
@@ -1,10 +1,13 @@
 package controller;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Computer {
     public static String[] instructionsMemory;
+    public static List<Integer> opcodeListFormatSize6 = new ArrayList<Integer>();  
     public static Map<String, Integer> memory = new LinkedHashMap<>();
     public static Map<String, Integer> registers = new HashMap<>();
 
@@ -12,8 +15,8 @@ public class Computer {
         instructionsMemory = new String[1000];
 
         createMemory(memory);
-
         createRegisters(registers);
+        fillOpcodeListSize6(opcodeListFormatSize6);
 
     }
 
@@ -54,10 +57,12 @@ public class Computer {
     }
 
     public void createMemory(Map<String, Integer> memory){
-        for(int i = 0; i <= 0xFFF; i++){
+        for(int i = 1; i <= 0xFFF; i++){
             String position = String.format("0x%03X", i);
-            memory.put(position, 0);
+            memory.put(position, i);
         }
+        memory.put("0x001", 2);
+        memory.put("0x002", 50);
     }
 
     public void createRegisters(Map<String, Integer> registers){
@@ -65,12 +70,53 @@ public class Computer {
         registers.put("X", 0);
         registers.put("L", 0);
         registers.put("B", 0);
+        registers.put("S", 0);
+        registers.put("T", 0);
         registers.put("PC", 0);
-        registers.put("SW", 0);    
-        registers.put("R1", 0);
-        registers.put("R2", 0);
+        registers.put("SW", 0); 
+
     }
 
+    public void fillOpcodeListSize6(List<Integer> opcodeList){
+        opcodeList.add(0x18);
+        opcodeList.add(0x40);
+        opcodeList.add(0x28);
+        opcodeList.add(0x24);
+        opcodeList.add(0x3C);
+        opcodeList.add(0x30);
+        opcodeList.add(0x34);
+        opcodeList.add(0x38);
+        opcodeList.add(0x48);
+        opcodeList.add(0x00);
+        opcodeList.add(0x68);
+        opcodeList.add(0x50);
+        opcodeList.add(0x08);
+        opcodeList.add(0x6C);
+        opcodeList.add(0x74);
+        opcodeList.add(0x04);
+        opcodeList.add(0x20);
+        opcodeList.add(0x44);
+        opcodeList.add(0x4C);
+        opcodeList.add(0x0C);
+        opcodeList.add(0x78);
+        opcodeList.add(0x54);
+        opcodeList.add(0x14);
+        opcodeList.add(0x7C);
+        opcodeList.add(0x84);
+        opcodeList.add(0x10);
+        opcodeList.add(0x1C);
+        opcodeList.add(0x2C);
+        opcodeList.add(0xB8);
+    }
+
+
+    public boolean verifyOpcodeSize6(int opCode){
+        if(opcodeListFormatSize6.contains(opCode))
+            return false;
+        if(opcodeListFormatSize6.contains(opCode << 2))
+            return true;
+        return false;
+    }
 
     
 }
--- src/model/Executor.java
+++ src/model/Executor.java
@@ -43,17 +43,17 @@ public class Executor{
             int instructionSize = getInstructionSize(instruction);
             
             switch (instructionSize) {
-                case 8:
+                /* case 8:
                     runInstructionFormat1(computer, instruction);
-                    break;
-                case 12:
+                    break; */
+                case 16:
                     runInstructionFormat2(computer, instruction);
                     break;
                 case 24:
-                    runInstructionFormat3(computer, instruction);
+                    runInstructionFormat3And4(computer, instruction);
                     break;
                 case 32:
-                    runInstructionFormat4(computer, instruction);
+                    runInstructionFormat3And4(computer, instruction);
                     break;
                 default:
                     System.out.println("Formato da seguinte instrução invalido: " + instruction);
@@ -65,7 +65,7 @@ public class Executor{
         }
     }
 
-    public static void runInstructionFormat1(Computer computer, String instruction){
+    /* public static void runInstructionFormat1(Computer computer, String instruction){
         int instructionToInt = Integer.parseInt(instruction, 16);
         int opCode = (instructionToInt & OPCODE_8BITS_MASK) >> 8;
 
@@ -76,69 +76,223 @@ public class Executor{
             default:
                 break;
         }
-    }
+    } */
 
     public static void runInstructionFormat2(Computer computer, String instruction){
         int instructionToInt = Integer.parseInt(instruction, 16);
         int opCode = (instructionToInt & OPCODE_8BITS_MASK) >> 8;
         int r1 = (instructionToInt & R1_MASK) >> 4;
-        int r2 = instructionToInt & R1_MASK;
+        int r2 = instructionToInt & R2_MASK;
 
+        String r1Key = getRegisterKey(r1);        
+        String r2Key = getRegisterKey(r2);
+
+        
         switch (opCode) {
-            case 0x18;
+            case 0x90: // ADDR
+                int addR = computer.readRegister(r1Key) + computer.readRegister(r2Key);
+                computer.writeRegister("R2", addR);
+                break;
+            case 0x04: // CLEAR
+                computer.writeRegister(r1Key, 0);
+                break;
+            case 0xA0: // COMPR
+                if(computer.readRegister(r1Key) > computer.readRegister(r2Key))
+                    computer.writeRegister(r2Key, 0x3E);
+                else if(computer.readRegister(r1Key) < computer.readRegister(r2Key))
+                    computer.writeRegister(r2Key, 0x3C);
+                else
+                    computer.writeRegister(r2Key, 0x3D);
+                break;
+            case 0x9C: // DIVR
+                int divR = computer.readRegister(r2Key) / computer.readRegister(r1Key);
+                computer.writeRegister("R2", divR);
+                break;
+            case 0x98: // MULTR
+                int multR = computer.readRegister(r1Key) * computer.readRegister(r2Key);
+                computer.writeRegister("R2", multR);
+                break;
+            case 0xAC: // RMO
+                int rmo = computer.readRegister(r1Key);
+                computer.writeRegister(r2Key, rmo);
+                break;
+            case 0xA4: // SHIFTL
+                int shiftL = computer.readRegister(r1Key) << r2;
+                computer.writeRegister(r1Key, shiftL);
+                break;
+            case 0xA8: // SHIFTR
+                int shiftR = computer.readRegister(r1Key) >> r2;
+                computer.writeRegister(r1Key, shiftR);
+                break;
+            case 0x94: // SUBR
+                int subR = computer.readRegister(r2Key) - computer.readRegister(r1Key);
+                computer.writeRegister("R2", subR);
+                break;
+            case 0xB8: // TIXR
+                if(computer.readRegister(r1Key) > computer.readRegister("X"))
+                    computer.writeRegister("SW", 0x3C);
+                else if(computer.readRegister(r1Key) < computer.readRegister("X"))
+                    computer.writeRegister("SW", 0x3E);
+                else
+                    computer.writeRegister("SW", 0x3D);
+                // incrementa 1 em X
+                computer.writeRegister("X", computer.readRegister("X") + 1);
                 break;
             default:
+                System.out.println("OpCode não encontrado: " + String.format("0x%03X", opCode));
                 break;
         }
     }
 
-    public static void runInstructionFormat3(Computer computer, String instruction){
+    public static void runInstructionFormat3And4(Computer computer, String instruction){
         int instructionToInt = Integer.parseInt(instruction, 16);
-        int opCode = (instructionToInt & OPCODE_MASK) >> 18;
-        int n = (instructionToInt & N_MASK_24) >> 17;
-        int i = (instructionToInt & I_MASK_24) >> 16;
-        int x = (instructionToInt & X_MASK_24) >> 15;
-        int b = (instructionToInt & B_MASK_24) >> 14;
-        int p = (instructionToInt & P_MASK_24) >> 13;
-        int e = (instructionToInt & E_MASK_24) >> 12;
-        int disp = instructionToInt & DISP_MASK;
+        int n, i, x, b, p, e, opCode, finalBits;
+        String finalAddress;
+        int value, finalValue;
+        if(getInstructionSize(instruction) == 24){
+            opCode = (instructionToInt & OPCODE_MASK) >> 18;
+            n = (instructionToInt & N_MASK_24) >> 17;
+            i = (instructionToInt & I_MASK_24) >> 16;
+            x = (instructionToInt & X_MASK_24) >> 15;
+            b = (instructionToInt & B_MASK_24) >> 14;
+            p = (instructionToInt & P_MASK_24) >> 13;
+            e = (instructionToInt & E_MASK_24) >> 12;
+            finalBits = instructionToInt & DISP_MASK; // DISP
+        } else {
+            opCode = (instructionToInt & OPCODE_MASK) >> 26;
+            n = (instructionToInt & N_MASK_32) >> 25;
+            i = (instructionToInt & I_MASK_32) >> 24;
+            x = (instructionToInt & X_MASK_32) >> 23;
+            b = (instructionToInt & B_MASK_32) >> 22;
+            p = (instructionToInt & P_MASK_32) >> 21;
+            e = (instructionToInt & E_MASK_32) >> 20;
+            finalBits = instructionToInt & ADDRESS_MASK;
+        }
 
         String flags = "" + n + i + x + b + p + e;
 
+        if(computer.verifyOpcodeSize6(opCode))
+            opCode = opCode << 2;
+
         switch (opCode) {
-            case 0x18:
-                computer.writeRegister("A", disp);
-                String finalAddress = calcAddress(computer, flags, disp);
+            case 0x18: // ADD
+                finalAddress = calcAddress(computer, flags, finalBits);
+                value = computer.readMemory(finalAddress);
+                finalValue = computer.readRegister("A") + value;
+                computer.writeRegister("A", finalValue);
+                break;
+            case 0x40: // AND
+                finalAddress = calcAddress(computer, flags, finalBits);
+                value = computer.readMemory(finalAddress);
+                finalValue = value & computer.readRegister("A");
+                computer.writeRegister("A", finalValue);
+                break;
+            case 0x28: // COMP
+                finalAddress = calcAddress(computer, flags, finalBits);
+                value = computer.readMemory(finalAddress);
+                if(value > computer.readRegister("A"))
+                    computer.writeRegister("SW", 0x3C);
+                else if(value < computer.readRegister("A"))
+                    computer.writeRegister("SW", 0x3E);
+                else
+                    computer.writeRegister("SW", 0x3D);
+
+                break;
+            case 0x24: // DIV
+                break; 
+            case 0x3C: // J
+                finalAddress = calcAddress(computer, flags, finalBits);
+                value = computer.readMemory(finalAddress);
+                computer.writeRegister("PC", value);
+                break;
+            case 0x30: // JEQ
+                if(computer.readRegister("SW") == 0x3D){
+                    finalAddress = calcAddress(computer, flags, finalBits);
+                    value = computer.readMemory(finalAddress);
+                    computer.writeRegister("PC", value);
+                }
+                break;
+            case 0x34: // JGT
+                if(computer.readRegister("SW") == 0x3E){
+                    finalAddress = calcAddress(computer, flags, finalBits);
+                    value = computer.readMemory(finalAddress);
+                    computer.writeRegister("PC", value);
+                }
+                break;
+            case 0x38: // JLT
+                if(computer.readRegister("SW") == 0x3C){
+                        finalAddress = calcAddress(computer, flags, finalBits);
+                        value = computer.readMemory(finalAddress);
+                        computer.writeRegister("PC", value);
+                    }
+                break;
+            case 0x48: // JSUB
+                finalAddress = calcAddress(computer, flags, finalBits);
+                value = computer.readMemory(finalAddress);
+                computer.writeRegister("L", computer.readRegister("PC"));
+                computer.writeRegister("PC", value);
+
+                break;
+            case 0x00: // LDA
+                break;
+            case 0x68: // LDB
+                break;
+            case 0x50: // LDCH
+                break;
+            case 0x08: // LDL
+                break;
+            case 0x6C: // LDS
+                break;
+            case 0x74: // LDT
+                break;
+            case 0x04: // LDX
+                break;
+            case 0x20: // MUL
+                break;
+            case 0x44: // OR
+                break;
+            case 0x4C: // RSUB
+                computer.writeRegister("PC", computer.readRegister("L"));
+                break;
+            case 0x0C: // STA
+                break;
+            case 0x78: // STB
+                break;
+            case 0x54: // STCH
+                break;
+            case 0x14: // STL
+                break;
+            case 0x7C: // STS
+                break;
+            case 0x84: // STT
+                break;
+            case 0x10: // STX
+                break;
+            case 0x1C: // SUB
+                finalAddress = calcAddress(computer, flags, finalBits);
+                value = computer.readMemory(finalAddress);
+                finalValue = computer.readRegister("A") - value;
+                computer.writeRegister("A", finalValue);
+                break;
+            case 0x2C: // TIX
+                finalAddress = calcAddress(computer, flags, finalBits);
+                value = computer.readMemory(finalAddress);
+                if(value > computer.readRegister("X"))
+                    computer.writeRegister("SW", 0x3C);
+                else if(value < computer.readRegister("X"))
+                    computer.writeRegister("SW", 0x3E);
+                else
+                    computer.writeRegister("SW", 0x3D);
+                // incrementa 1 em X
+                computer.writeRegister("X", computer.readRegister("X") + 1);
                 break;
             default:
+                System.out.println("OpCode não encontrado: " + String.format("0x%03X", opCode));
                 break;
         }
 
     }
     
-    public static void runInstructionFormat4(Computer computer, String instruction){
-        int instructionToInt = Integer.parseInt(instruction, 16);
-        int opCode = (instructionToInt & OPCODE_MASK) >> 26;
-        int n = (instructionToInt & N_MASK_32) >> 25;
-        int i = (instructionToInt & I_MASK_32) >> 24;
-        int x = (instructionToInt & X_MASK_32) >> 23;
-        int b = (instructionToInt & B_MASK_32) >> 22;
-        int p = (instructionToInt & P_MASK_32) >> 21;
-        int e = (instructionToInt & E_MASK_32) >> 20;
-        int address = instructionToInt & ADDRESS_MASK;
-        String flags = "" + n + i + x + b + p + e;
-
-        switch (opCode) {
-            case 0x18:
-                String finalAddress = calcAddress(computer, flags, address);
-                
-                break;
-            default:
-                break;
-        }
-
-
-    }
 
     public static String calcAddress(Computer computer, String flags, int lastBits){
         String finalAddress;
@@ -147,64 +301,64 @@ public class Executor{
         switch (flags) {
             // Endereçamento direto
             case "110000":
-                finalAddress = String.format("0x03X", lastBits);
+                finalAddress = String.format("0x%03X", lastBits);
                 return finalAddress;
             case "110001":
-                finalAddress = String.format("0x03X", lastBits);
+                finalAddress = String.format("0x%03X", lastBits);
                 return finalAddress;
             case "110010":
                 calc = computer.readRegister("PC") + lastBits;
-                finalAddress = String.format("0x03X", calc);
+                finalAddress = String.format("0x%03X", calc);
                 return finalAddress;
             case "110100":
                 calc = computer.readRegister("B") + lastBits;
-                finalAddress = String.format("0x03X", calc);
+                finalAddress = String.format("0x%03X", calc);
                 return finalAddress;
             case "111000":
                 calc = computer.readRegister("X") + lastBits;
-                finalAddress = String.format("0x03X", calc);
+                finalAddress = String.format("0x%03X", calc);
                 return finalAddress;
             case "111001":
                 calc = computer.readRegister("X") + lastBits;
-                finalAddress = String.format("0x03X", calc);
+                finalAddress = String.format("0x%03X", calc);
                 return finalAddress;
             case "111010":
                 calc = computer.readRegister("PC") + lastBits + computer.readRegister("X");
-                finalAddress = String.format("0x03X", calc);
+                finalAddress = String.format("0x%03X", calc);
                 return finalAddress;
             case "111100":
                 calc = computer.readRegister("B") + lastBits + computer.readRegister("X");
-                finalAddress = String.format("0x03X", calc);
+                finalAddress = String.format("0x%03X", calc);
                 return finalAddress;
             // Endereçamento indireto
             case "100000":
-                midAddress = String.format("0x03X", lastBits);
-                finalAddress = String.format("0x03X", computer.readMemory(midAddress));
+                midAddress = String.format("0x%03X", lastBits);
+                finalAddress = String.format("0x%03X", computer.readMemory(midAddress));
                 return finalAddress;
             case "100001":
-                midAddress = String.format("0x03X", lastBits);
-                finalAddress = String.format("0x03X", computer.readMemory(midAddress));
+                midAddress = String.format("0x%03X", lastBits);
+                finalAddress = String.format("0x%03X", computer.readMemory(midAddress));
                 return finalAddress;
             case "100010":
                 calc = computer.readRegister("PC") + lastBits;
-                midAddress = String.format("0x03X", calc);
-                finalAddress = String.format("0x03X", computer.readMemory(midAddress));
+                midAddress = String.format("0x%03X", calc);
+                finalAddress = String.format("0x%03X", computer.readMemory(midAddress));
                 return finalAddress;
             case "100100":
                 calc = computer.readRegister("B") + lastBits;
-                midAddress = String.format("0x03X", calc);
-                finalAddress = String.format("0x03X", computer.readMemory(midAddress));
+                midAddress = String.format("0x%03X", calc);
+                finalAddress = String.format("0x%03X", computer.readMemory(midAddress));
             // Endereçamento imediato
             case "010000":
-                return String.format("0x03X", lastBits);
+                return String.format("0x%03X", lastBits);
             case "010001":
-                return String.format("0x03X", lastBits);
+                return String.format("0x%03X", lastBits);
             case "010010":
                 calc = computer.readRegister("PC") + lastBits;
-                return String.format("0x03X", lastBits);
+                return String.format("0x%03X", lastBits);
             case "010100":
                 calc = computer.readRegister("PC") + lastBits;
-                return String.format("0x03X", lastBits);
+                return String.format("0x%03X", lastBits);
             default:
                 System.out.println("Flags não encontradas: " + flags);
                 break;
@@ -222,4 +376,30 @@ public class Executor{
         
         return binary.toString().length();
     }
+
+    public static String getRegisterKey(int register){
+        switch (register) {
+            case 0:
+                return "A";
+            case 1: 
+                return "X";
+            case 2:
+                return "L";
+            case 3:
+                return "B";
+            case 4:
+                return "S";
+            case 5:
+                return "T";
+            case 8:
+                return "PC";
+            case 9:
+                return "SW";
+            default:
+                System.out.println("Registrador não encontrado: " + register);
+                return "-100";
+        }
+    }
+
 }
+
