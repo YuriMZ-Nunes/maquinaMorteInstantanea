55f60c3dfcc703f9ce3738ac6dc94a0e3fdcc382
YuriMZ-Nunes <yuri.m.z.nunes@gmail.com>
2023-12-01 22:45:34 -0300

    commit

--- src/model/App.java
+++ src/model/App.java
@@ -12,15 +12,6 @@ public class App {
             Loader.loadProgram(computer, "lib/program.txt");
             Executor.executeProgram(computer);
 
-            int finalAregisterValue = computer.readRegister("A");
-            int finalXregisterValue = computer.readRegister("X");
-            int finalLregisterValue = computer.readRegister("L");
-            int finalBregisterValue = computer.readRegister("B");
-            int finalSregisterValue = computer.readRegister("S");
-            int finalTregisterValue = computer.readRegister("T");
-            int finalPCregisterValue = computer.readRegister("PC");
-            int finalSWregisterValue = computer.readRegister("SW");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
