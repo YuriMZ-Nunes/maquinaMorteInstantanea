19769244d92ccde1e5b16e549273394a95e4bbe8
Andersson <89083903+anderssonslv@users.noreply.github.com>
2023-12-02 21:38:38 -0300

    Atualizando Interface
    Tentando arrumar os eventos para carregar arquivos e executar...

--- src/view/InterfaceSwing.java
+++ src/view/InterfaceSwing.java
@@ -1,27 +1,73 @@
 package view;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import controller.*;
+import model.*;
 
 public class InterfaceSwing extends JFrame {
 
-    public InterfaceSwing() {
+    public InterfaceSwing(Computer C) {
+        Computer system = C;
+
         // Configurações da janela principal
         setTitle("Interface Swing");
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(2, 2)); // Usando GridLayout com 2 linhas e 2 colunas
 
-        // Painel superior esquerdo com um botão "Carregar"
-        JPanel panelSuperiorEsquerdo = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        JButton btnCarregar = new JButton("Carregar");
-        panelSuperiorEsquerdo.add(btnCarregar);
 
-        // Painel superior direito com o valor de 5 variáveis
-        JPanel panelSuperiorDireito = new JPanel(new GridLayout(5, 2));
-        JLabel[] registerLabels = new JLabel[5];
-        JLabel[] registerValues = new JLabel[5];
+
+
+
+        // Painel superior esquerdo Carregar Arq/Executar
+        JPanel panelSuperiorEsquerdo = new JPanel();
+        panelSuperiorEsquerdo.setBorder(BorderFactory.createTitledBorder("Arquivo"));
+        GridBagLayout gbl = new GridBagLayout();
+        panelSuperiorEsquerdo.setLayout(gbl);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbl.rowHeights = new int[] { 50, 50 };  gbl.columnWidths = new int[] { 50, 100 };
+        JButton btnLoad = new JButton("Carregar");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        panelSuperiorEsquerdo.add(btnLoad, gbc);
+
+        JButton btnExec = new JButton("Executar");
+        btnExec.setEnabled(false);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panelSuperiorEsquerdo.add(btnExec, gbc);
+        JTextArea textAreaInput = new JTextArea();
+            JScrollPane scrollPane = new JScrollPane(textAreaInput);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+                gbc.gridx = 1;
+                gbc.gridy = 0;
+                gbc.insets = new Insets(0, 10, 0, 0);
+                gbc.gridheight = 2;
+                gbc.gridwidth = 2;
+                gbc.fill = GridBagConstraints.BOTH;
+                textAreaInput.setLineWrap(true);
+            scrollPane.setPreferredSize(new Dimension(50, 200));
+        panelSuperiorEsquerdo.add(scrollPane, gbc);
+
+
+
+
+
+        // Painel superior direito com o valor de 7 registradores
+        JPanel panelSuperiorDireito = new JPanel(new GridLayout(8, 2));
+        panelSuperiorDireito.setBorder(BorderFactory.createTitledBorder("Registradores"));
+
+        JLabel[] registerLabels = new JLabel[8];
+        JLabel[] registerValues = new JLabel[8];
 
         for (int i = 0; i < registerValues.length; i++)
             registerValues[i] = new JLabel("Valor");
@@ -29,40 +75,110 @@ public class InterfaceSwing extends JFrame {
         registerLabels[0] = new JLabel("A:");
         registerLabels[1] = new JLabel("X:");
         registerLabels[2] = new JLabel("L:");
-        registerLabels[3] = new JLabel("PC:");
-        registerLabels[4] = new JLabel("SW:");
+        registerLabels[3] = new JLabel("B:");
+        registerLabels[4] = new JLabel("S:");
+        registerLabels[5] = new JLabel("T:");
+        registerLabels[6] = new JLabel("PC:");
+        registerLabels[7] = new JLabel("SW:");
 
         for (int i = 0; i < registerLabels.length; i++) {
             panelSuperiorDireito.add(registerLabels[i]);
             panelSuperiorDireito.add(registerValues[i]);
         }
-        
-        // Painel inferior esquerdo com um campo de texto para endereços de memória
-        JPanel panelInferiorEsquerdo = new JPanel();
-        JTextArea textAreaEnderecos = new JTextArea(10, 20);
-        panelInferiorEsquerdo.add(new JScrollPane(textAreaEnderecos));
 
-        // Painel inferior direito com campos de entrada e saída
-        JPanel panelInferiorDireito = new JPanel(new GridLayout(2, 2));
+
+
+
+
+        // Painel inferior esquerdo com campos de entrada e saída
+        JPanel panelInferiorEsquerdo = new JPanel(new GridLayout(2, 2));
+        panelInferiorEsquerdo.setBorder(BorderFactory.createTitledBorder("I/O"));
+
         JTextField textFieldInput = new JTextField();
         JTextField textFieldOutput = new JTextField();
-        panelInferiorDireito.add(new JLabel("Input:"));
-        panelInferiorDireito.add(textFieldInput);
-        panelInferiorDireito.add(new JLabel("Output:"));
-        panelInferiorDireito.add(textFieldOutput);
+        panelInferiorEsquerdo.add(new JLabel("Input:"));
+        panelInferiorEsquerdo.add(textFieldInput);
+        panelInferiorEsquerdo.add(new JLabel("Output:"));
+        panelInferiorEsquerdo.add(textFieldOutput);
+
+
+
+
+        
+        // Painel inferior direito com um campo de texto para endereços de memória
+        JPanel panelInferiorDireito = new JPanel();
+        panelInferiorDireito.setBorder(BorderFactory.createTitledBorder("Memória"));
+
+        DefaultTableModel model = new DefaultTableModel();
+            model.addColumn("Endereço");
+            model.addColumn("Valor");
+
+        for (int i = 0; i < 100; i++) 
+            model.addRow(new Object[]{"0x00"+i, i+1024+""+i+128});
+
+        JTable table = new JTable(model);
+            table.getColumnModel().getColumn(0).setPreferredWidth(100);
+            table.getColumnModel().getColumn(1).setPreferredWidth(100); 
+
+        JScrollPane scroll = new JScrollPane(table);
+            scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        table.setPreferredScrollableViewportSize(new Dimension(200, 175));
+        table.setEnabled(false);
+        panelInferiorDireito.add(new JScrollPane(table));
+
+
+
+
 
         // Adicionando os painéis à janela principal
         add(panelSuperiorEsquerdo);
         add(panelSuperiorDireito);
-        add(panelInferiorEsquerdo);
         add(panelInferiorDireito);
+        add(panelInferiorEsquerdo);
+
 
         // Ação do botão "Carregar"
-        btnCarregar.addActionListener(new ActionListener() {
+        StringBuilder file = new StringBuilder();
+        btnLoad.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Coloque aqui a lógica para carregar algo
-                JOptionPane.showMessageDialog(InterfaceSwing.this, "Botão Carregar pressionado!");
+                JFileChooser fileChooser = new JFileChooser();
+                int window = fileChooser.showOpenDialog(null);
+                if (window == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    file.append(selectedFile.getAbsolutePath());
+                    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+                    
+                    try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            //file.append(line+"\n");
+                            textAreaInput.append(line + "\n");
+                            btnExec.setEnabled(true);
+                        }
+                    } 
+                    catch (IOException exception) {
+                        System.out.println("Error: " + exception.getMessage());
+                    }
+                }
+            }
+        });
+                                
+
+
+        btnExec.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.out.println(file.toString());
+                String locationTxt = file.toString();
+                try {
+                    Loader.loadProgram(C, "lib/program.txt");
+                    Executor.executeProgram(C);
+                    System.out.println("Loader carregado com sucesso!");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
     }
@@ -71,9 +187,16 @@ public class InterfaceSwing extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                InterfaceSwing interfaceSwing = new InterfaceSwing();
+                Computer C = new Computer(1000); //TESTE
+                InterfaceSwing interfaceSwing = new InterfaceSwing(C);
                 interfaceSwing.setVisible(true);
             }
         });
     }
 }
+
+
+
+
+
+
